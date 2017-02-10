9880b092ae625ffcc57a909cc1fdfcf003be2e17
Aamir khan <am.khan1996@gmail.com>
2017-02-10 18:16:44 +0530

    Added form validation to index.php

--- addEvents.js
+++ addEvents.js
@@ -1,12 +1,43 @@
-// var databaseRef = firebase.database().ref().child('message');
+//getting upload button.
+var uploadBtn = document.getElementById('uploadBtn');
+//listen for file selection.
+uploadBtn.addEventListener('change', function(e) {
+	var eventName = document.getElementById('eventName');
+	//get the file.
+	var file = e.target.files[0];
+	//creating a storage reference.
+	var storageRef = firebase.storage().ref('posters/' + file.name);
+	//upload the file.
+	var uploadTask = storageRef.put(file);
+	//register three observers
+	uploadTask.on('state_changed', function(snapshot) {
+		var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+		console.log('Upload is ' + progress + '% done.');
+		switch (snapshot.state) {
+		    case firebase.storage.TaskState.PAUSED: // or 'paused'
+		      console.log('Upload is paused');
+		      break;
+		    case firebase.storage.TaskState.RUNNING: // or 'running'
+		      console.log('Upload is running');
+		      break;
+		  }
+	}, function(error) {
 
-// databaseRef.on('value', function(snapshot) {
-// 	document.getElementById('heading').innnerText = snapshot.val();
-// });
+	}, function() {
+		//getting the poster url.
+		posterURL = uploadTask.snapshot.downloadURL;
+		//getting the database reference.
+		var dbRef = firebase.database();
+		//pushing posterURL on events details in database.
+		dbRef.ref('events/' + eventName.value).set({
+			posterURL: posterURL
+		});
+		document.getElementById('createEvent').disabled = false;
+	});
+});
 
 //adding data to the database.
 function addNewEvent() {
-	
 	//storing inputs into variables.
 	var eventName = document.getElementById('eventName');
 	var studentBody = document.getElementById('studentBody');
@@ -18,10 +49,14 @@ function addNewEvent() {
 	var eventDesc = document.getElementById('eventDesc');
 	var reward = document.getElementById('reward');
 
+	if (eventName.value.length == 0 || studentBody.value.length == 0 || date.value.length == 0 || fee.value.length == 0 || contactPerson.value.length == 0 || contact.value.length == 0 || location.value.length == 0 || eventDesc.value.length == 0 || reward.value.length == 0) {
+		return;
+	}
+
 	//getting a database service reference.
 	var dbRef = firebase.database();
 
-	dbRef.ref('events/' + eventName.value).set({
+	dbRef.ref('events/' + eventName.value).update({
 		name: eventName.value,
 		studentBody: studentBody.value,
 		date: date.value,
@@ -34,4 +69,5 @@ function addNewEvent() {
 		isApproved: false,
 		votes: 0
 	});
+
 }
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php
-// include 'firebaseInterface.php';
-// include 'firebaseLib.php';
-// include 'firebaseStub.php';
-?>
+
+<!--  include 'firebaseInterface.php';
+ include 'firebaseLib.php';
+ include 'firebaseStub.php'; -->
+
 <!DOCTYPE html>
 
 <html>
@@ -12,8 +12,14 @@
         <title>Event Register</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
+
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/additional-methods.min.js"></script>
 
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
@@ -24,19 +30,20 @@
             <div class="container">
                 
                 <div class="well">
-                    <!-- <form> -->
+
+                    <form method="POST" id="createEventForm">
 
                         <div class="form-group">
                             <label class="control-label col-sm-3">Event Name:</label>
                             <div class="col-sm-9">
-                                <input id="eventName" class="form-control" type="text" name="eventName" />
+                                <input id="eventName" class="form-control" type="text" name="eventName"  required="" />
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="control-label col-sm-3">Student body:</label>
                             <div class="col-sm-9">
-                                <select class="form-control" id="studentBody">
+                                <select class="form-control" id="studentBody" name="studentBody">
                                     <option value="CSI">CSI</option>
                                     <option value="ECELL">ECELL</option>
                                     <option value="IEEE">IEEE</option>
@@ -48,29 +55,29 @@
                         <div class="form-group">
                             <label class="control-label col-sm-3">Date:</label>
                             <div class="col-sm-9">
-                                <input id="date" class="form-control" type="date" name="date" />
+                                <input id="date" class="form-control" type="date" name="date" required="" />
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="control-label col-sm-3">Registration Fee:</label>
                             <div class="col-sm-9">
-                                <input id="fee" class="form-control" type="text" name="fee" />
+                                <input id="fee" class="form-control" type="text" name="fee" required="" />
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="control-label col-sm-3">Contact:</label>
                             <div class="col-sm-9">
-                                <input id="contactPerson" class="form-control" type="text" name="contactPerson" style="width: 50%" />:
-                                <input id="contact" class="form-control" type="tel" name="contact" style="width: 50%" />
+                                <input id="contactPerson" class="form-control" type="text" name="contactPerson" style="width: 50%" required="" />:
+                                <input id="contact" class="form-control" type="tel" name="contact" style="width: 50%" required="" />
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="control-label col-sm-3">Location:</label>
                             <div class="col-sm-9">
-                                <input id="location" class="form-control" type="text" name="location" />
+                                <input id="location" class="form-control" type="text" name="location" required="" />
                             </div>
                         </div>
 
@@ -84,14 +91,23 @@
                         <div class="form-group">
                             <label class="control-label col-sm-3">Reward:</label>
                             <div class="col-sm-9">
-                                <input id="reward" class="form-control" type="text" name="reward" />
+                                <input id="reward" class="form-control" type="text" name="reward" required="" />
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <input id="createEvent" class="btn btn-default" onclick="addNewEvent()" type="submit" name="submit" value="CREATE EVENT"  />
+                            <label class="control-label col-sm-3">Poster:</label>
+                            <div class="col-sm-9">
+                                <input type="file" name="poster" accept="image/*" class="btn btn-default" id="uploadBtn" required="" />
+                            </div>
                         </div>
-                    <!-- </form>         -->
+
+                        <div class="form-group">
+                            <input id="createEvent" class="btn btn-default" onclick="addNewEvent()" type="submit" name="submit" value="CREATE EVENT" required="" disabled="true" />
+                        </div>
+
+                    </form>
+
                 </div>
 
             </div>
